bec02ce0fde674421ec9cead98d5f9ab6d319492
Aggelosk <akermezo@yahoo.com>
2025-02-13 12:18:33 +0200

    Add project to repo

---
+++ PleaseDo/PleaseDo/PleaseDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleaseDoApp.swift
+//  PleaseDo
+//
+//  Created by Anxhelo Kermezo on 13/2/25.
+//
+
+import SwiftUI
+
+@main
+struct PleaseDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PleaseDo/PleaseDoTests/PleaseDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PleaseDoTests.swift
+//  PleaseDoTests
+//
+//  Created by Anxhelo Kermezo on 13/2/25.
+//
+
+import Testing
+@testable import PleaseDo
+
+struct PleaseDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
