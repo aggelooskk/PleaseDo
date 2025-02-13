8eaaf3ac88af2c0c20add90dcd6e53c9c5bdb83d
Aggelosk <akermezo@yahoo.com>
2025-02-13 17:00:33 +0200

    Add ListView to project.

---
+++ PleaseDo/PleaseDo/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  PleaseDo
+//
+//  Created by Anxhelo Kermezo on 13/2/25.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
