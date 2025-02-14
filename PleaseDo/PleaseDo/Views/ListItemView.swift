f3ddfb874eb1761cf6ee523975b9bb351a9658c9
Aggelosk <akermezo@yahoo.com>
2025-02-14 17:13:37 +0200

    Add basic UI to ListItemView.

--- PleaseDo/PleaseDo/Views/ListItemView.swift
+++ PleaseDo/PleaseDo/Views/ListItemView.swift
@@ -12,10 +12,29 @@ struct ListItemView: View {
     let height: CGFloat
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 10) {
+            VStack(alignment: .leading, spacing: 3) {
+                Text("Title")
+                    .font(.headline)
+                    .lineLimit(1)
+                
+                Text("Some description goes here")
+                    .font(.body)
+                    .foregroundStyle(.secondary)
+                    .lineLimit(2)
+            }
+            
+            Spacer()
+            
+            Image(systemName: "circle")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.blue)
+                .frame(maxHeight: height * 0.45)
+        }
     }
 }
 
-#Preview {
+#Preview(traits: .sizeThatFitsLayout) {
     ListItemView(item: Item(id: "abc123", authorId: "John Doe", title: "First item", description: "First Description", startDate: .now + 5, status: .todo, priority: .low), height: 100)
 }
