80c5ea80832a43c24603273b2a6f963db45b1ece
Aggelosk <akermezo@yahoo.com>
2025-02-16 17:58:47 +0200

    Add color extensions.

---
+++ PleaseDo/PleaseDo/App/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  PleaseDo
+//
+//  Created by Anxhelo Kermezo on 16/2/25.
+//
+
+import SwiftUI
+
+extension Color {
+    static let background = Color("MainBackground", bundle: nil)
+    static let dullBlue = Color("Blueberry", bundle: nil)
+    static let lightBlue = Color("DarkSky", bundle: nil)
+    static let lightGray = Color("Sliver", bundle: nil)
+}
