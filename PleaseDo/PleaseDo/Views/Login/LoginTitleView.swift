d4025099cfed6e01c27762f803546c1dc981b992
Aggelosk <akermezo@yahoo.com>
2025-02-19 18:21:53 +0200

    Add LoginView and LoginTitleView to project.

---
+++ PleaseDo/PleaseDo/Views/Login/LoginTitleView.swift
@@ -0,0 +1,40 @@
+//
+//  LoginTitleView.swift
+//  PleaseDo
+//
+//  Created by Anxhelo Kermezo on 19/2/25.
+//
+
+import SwiftUI
+
+struct LoginTitleView: View {
+    var body: some View {
+        HStack() {
+            Image("PleaseDoLogo")
+                .resizable()
+                .scaledToFit()
+            
+            VStack(spacing: 3) {
+                Text("Please Do")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                
+                RoundedRectangle(cornerRadius: 5)
+                    .frame(maxWidth: 175,
+                           maxHeight: 0.7)
+                    .foregroundStyle(.gray)
+                
+                Text("A better To Do List")
+                    .font(.subheadline)
+                    .fontWeight(.medium)
+                    .foregroundStyle(.secondary)
+            }
+        }
+        .frame(maxHeight: 110)
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    LoginTitleView()
+}
---
+++ PleaseDo/PleaseDo/Views/Login/LoginView.swift
@@ -0,0 +1,24 @@
+//
+//  LoginView.swift
+//  PleaseDo
+//
+//  Created by Anxhelo Kermezo on 19/2/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 15) {
+            Spacer()
+            
+            LoginTitleView()
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
