90f8e4358a8e54e7807e615a8b5bf5d46f0dc59f
Aggelosk <akermezo@yahoo.com>
2025-02-20 11:34:10 +0200

    LoginView and EmailTextField  building.

--- PleaseDo/PleaseDo/Views/Login/LoginFields.swift
+++ PleaseDo/PleaseDo/Views/Login/LoginFields.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 struct LoginFields: View {
+    @State private var email: String = ""
+    
     var body: some View {
         VStack(spacing: 12) {
-            
+            EmailTextField(placeholder: "user@mail.com", text: $email)
         }
+        .padding(.horizontal)
     }
 }
 
