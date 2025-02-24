21a29b56a7adf1a916e67741d0ec4dc94d6f0cd4
Aggelosk <akermezo@yahoo.com>
2025-02-24 11:02:41 +0200

    Add if statement for logging in and signup toggle in LoginView.

--- PleaseDo/PleaseDo/Views/Login/LoginView.swift
+++ PleaseDo/PleaseDo/Views/Login/LoginView.swift
@@ -23,7 +23,11 @@ struct LoginView: View {
                 .fontWeight(.semibold)
                 .padding(.horizontal)
             
-            LoginFields()
+            if isLoggingIn {
+                LoginFields()
+            } else {
+                SignUpFields()
+            }
             
             HStack {
                 Spacer()
