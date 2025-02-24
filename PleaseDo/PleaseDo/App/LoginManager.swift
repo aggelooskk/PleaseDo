a4c759e6a597834aa8ffc8d46aa41601fbdeed04
Aggelosk <akermezo@yahoo.com>
2025-02-24 12:12:56 +0200

    Add LoginManager and connect Firebase to project.

---
+++ PleaseDo/PleaseDo/App/LoginManager.swift
@@ -0,0 +1,13 @@
+//
+//  LoginManager.swift
+//  PleaseDo
+//
+//  Created by Anxhelo Kermezo on 24/2/25.
+//
+
+import Foundation
+import FirebaseAuth
+
+final class LoginManager {
+    // fetch login related data to pass to LoginVM
+}
