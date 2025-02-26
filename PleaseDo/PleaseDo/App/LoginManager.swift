fefffa7d0bf83ee9bc98d1e0d96e622516916a9b
Aggelosk <akermezo@yahoo.com>
2025-02-26 18:42:16 +0200

    Refactor to async await signup function.

--- PleaseDo/PleaseDo/App/LoginManager.swift
+++ PleaseDo/PleaseDo/App/LoginManager.swift
@@ -16,29 +16,47 @@ final class LoginManager {
     var currentUser: User?
     
     func signUp(_ fname: String, _ lname: String, _ email: String, _ pw: String) {
-        Auth.auth().createUser(withEmail: email, password: pw) { [weak self] result, error in
-            if let error {
+        Task {
+            do {
+                let result = try await Auth.auth().createUser(withEmail: email, password: pw)
+                currentUser = result.user
+                let id = result.user.uid
+                try await db.collection("Users").document(id).setData([
+                  "fname": fname,
+                  "lname": lname,
+                  "email": email,
+                  "id": id
+                ])
+                print("Document succesfully written")
+            } catch {
                 print(error)
-                return
-            }
-            guard let self, let result else { return }
-            currentUser = result.user
-            let id = result.user.uid
-            
-            Task { [weak self] in
-                guard let self else { return }
-                do {
-                  try await db.collection("Users").document(id).setData([
-                    "fname": fname,
-                    "lname": lname,
-                    "email": email,
-                    "id": id
-                  ])
-                  print("Document successfully written!")
-                } catch {
-                  print("Error writing document: \(error)")
-                }
             }
         }
-    }
-}
+        
+        
+//        Auth.auth().createUser(withEmail: email, password: pw) { [weak self] result, error in
+//            if let error {
+//                print(error)
+//                return
+//            }
+//            guard let self, let result else { return }
+//            currentUser = result.user
+//            let id = result.user.uid
+//
+//            Task { [weak self] in
+//                guard let self else { return }
+//                do {
+//                  try await db.collection("Users").document(id).setData([
+//                    "fname": fname,
+//                    "lname": lname,
+//                    "email": email,
+//                    "id": id
+//                  ])
+//                  print("Document successfully written!")
+//                } catch {
+//                  print("Error writing document: \(error)")
+//                }
+//            }
+//        }
+//    }
+//}
