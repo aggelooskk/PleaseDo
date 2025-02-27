8fe1fb19319060e7eb3ba56e78d2df15451a5eb4
Aggelosk <akermezo@yahoo.com>
2025-02-27 11:46:54 +0200

    LoginManager fixes.

--- PleaseDo/PleaseDo/App/LoginManager.swift
+++ PleaseDo/PleaseDo/App/LoginManager.swift
@@ -22,41 +22,15 @@ final class LoginManager {
                 currentUser = result.user
                 let id = result.user.uid
                 try await db.collection("Users").document(id).setData([
-                  "fname": fname,
-                  "lname": lname,
-                  "email": email,
-                  "id": id
+                    "fname": fname,
+                    "lname": lname,
+                    "email": email,
+                    "id": id
                 ])
                 print("Document succesfully written")
             } catch {
                 print(error)
             }
         }
-        
-        
-//        Auth.auth().createUser(withEmail: email, password: pw) { [weak self] result, error in
-//            if let error {
-//                print(error)
-//                return
-//            }
-//            guard let self, let result else { return }
-//            currentUser = result.user
-//            let id = result.user.uid
-//
-//            Task { [weak self] in
-//                guard let self else { return }
-//                do {
-//                  try await db.collection("Users").document(id).setData([
-//                    "fname": fname,
-//                    "lname": lname,
-//                    "email": email,
-//                    "id": id
-//                  ])
-//                  print("Document successfully written!")
-//                } catch {
-//                  print("Error writing document: \(error)")
-//                }
-//            }
-//        }
-//    }
-//}
+    }
+}
