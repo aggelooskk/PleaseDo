60f8297b9836105dc16238a5f748bc0324584b03
Aggelosk <akermezo@yahoo.com>
2025-02-27 12:15:24 +0200

    Setup Auth State Listener, RemoverListener and SignOut function.

--- PleaseDo/PleaseDo/App/LoginManager.swift
+++ PleaseDo/PleaseDo/App/LoginManager.swift
@@ -13,7 +13,27 @@ final class LoginManager {
     // fetch login related data to pass to LoginVM
     
     private let db = Firestore.firestore()
+    private var handler: AuthStateDidChangeListenerHandle?
     var currentUser: User?
+    
+    init() {
+        setupListener()
+    }
+    
+    deinit {
+        removeListener()
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+            currentUser = nil
+            removeListener()
+            print)"Succesfully sign out user"
+        } catch {
+            print(error)
+        }
+    }
     
     func signUp(_ fname: String, _ lname: String, _ email: String, _ pw: String) {
         Task {
@@ -31,6 +51,21 @@ final class LoginManager {
             } catch {
                 print(error)
             }
+        }
+    }
+    
+    func setupListener() {
+        guard handler == nil else { return }
+        
+        handler = Auth.auth().addStateDidChangeListener { [weak self] auth, user in
+            guard let self else { return }
+            currentUser = auth.currentUser
+        }
+    }
+    
+    func removeListener() {
+        if let h = handler {
+            Auth.auth().removeStateDidChangeListener(h)
         }
     }
 }
