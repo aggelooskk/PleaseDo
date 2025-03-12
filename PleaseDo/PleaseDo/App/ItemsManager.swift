4c567c4927a23e434f9a1614620cac2d60c68bee
Aggelosk <akermezo@yahoo.com>
2025-03-12 16:59:14 +0200

    Add ItemsManager to project.

---
+++ PleaseDo/PleaseDo/App/ItemsManager.swift
@@ -0,0 +1,16 @@
+//
+//  ItemsManager.swift
+//  PleaseDo
+//
+//  Created by Anxhelo Kermezo on 12/3/25.
+//
+
+import Foundation
+
+
+final class ItemsManager {
+    static let shared = ItemsManager()
+    private init() {}
+    
+    
+}
