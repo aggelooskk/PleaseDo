52cced0501cc7c4da878692881c49662388a1d43
Aggelosk <akermezo@yahoo.com>
2025-03-17 18:22:18 +0200

    finishing writing data to firestore

--- PleaseDo/PleaseDo/ViewModels/NewItemVM.swift
+++ PleaseDo/PleaseDo/ViewModels/NewItemVM.swift
@@ -6,10 +6,23 @@
 //
 
 import Foundation
+import FirebaseAuth
 
 final class NewItemVM: @ObservableObject {
     
     @Published var newItem = Item.empty()
+    
+    init() {
+        guard let user = Auth.auth().currentUser else { return }
+        newItem = Item(id: UUID().uuidString,
+            authorId: user.uid,
+            title: "",
+            description: "",
+            startDate: .now,
+            status: .todo,
+            priority: .low
+        )
+    }
     
     func saveNewItem() async throws {
        try await  IM.shared.save(newItem)
