71c2e41ec497d958e0a45cc7d72d48e278f8692e
Aggelosk <akermezo@yahoo.com>
2025-03-18 19:13:47 +0200

    Fixing keyboard focus logic in loginFileds and SignupFields

--- PleaseDo/PleaseDo/Views/Login/LoginFields.swift
+++ PleaseDo/PleaseDo/Views/Login/LoginFields.swift
@@ -11,15 +11,39 @@ struct LoginFields: View {
     @Binding var email: String
     @Binding var pw: String
     
+    enum Field: Hashable {
+        case email, pw
+    }
+    @FocusState private var isKeyboardFocused: Field?
+    
     var body: some View {
         VStack(spacing: 12) {
             EmailTextField(placeholder: "user@mail.com", text: $email)
                 .shadow(color: .gray, radius: 4, x: 1, y: 2.5)
+                .focused($isKeyboardFocused, equals: .email)
             
             PasswordField(placeholder: "pasword", text: $pw)
                 .shadow(color: .gray, radius: 4, x: 1, y: 2.5)
+                .focused($isKeyboardFocused, equals: .pw)
         }
         .padding(.horizontal)
+        .toolbar {
+            ToolbarItemGroup(placement: .keyboard) {
+                Button("Cancel") {
+                    guard let isKeyboardFocused else { return }
+                    switch isKeyboardFocused {
+                    case .email:
+                        email = ""
+                    case .pw:
+                        pw = ""
+                    }
+                }
+                
+                Button("Done") {
+                    isKeyboardFocused = nil
+                }
+            }
+        }
     }
 }
 
